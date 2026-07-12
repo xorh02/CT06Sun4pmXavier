c1577c16d79a4d30f8e1313d6513b03911a87527
xorh02 <xavierong02@gmail.com>
2026-07-12 07:48:18 +0000

    Jul 12, 2026, 7:48 AM

--- L01_redcircle.js
+++ L01_redcircle.js
@@ -1,11 +1,24 @@
 function setup(){
-    createCanvas(600, 400);
-    background(220);
+    createCanvas(600, 400);//width , height
+    background(220); //
 }
 
 function draw(){
-    fill(255, 0, 0);
-    ellipse(300,200,200,200);
+    fill(255, 0, 0); // fill(red,green,blue);
+    ellipse(300,100,100,100); //ellipse(x position, y position,width,height) 
     
-    // Todo: Challenge 3 Traffic Light
+    // Todo: Challenge 3 Traffic Light create two more circle 
+    //circle 1
+    fill(255,255,0);
+    ellipse(300,200,100,100);
+    //circle 2
+    
+    fill(0,255,0);
+    ellipse(300,300,100,100);
+    
+    fill(0,255,0);
+    ellipse(200,200,100,100); 
+
+     fill(0,255,0);
+    ellipse(400,200,100,100);
 }
