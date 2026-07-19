af406573f46887a58b86945d373fd25430a35a56
xorh02 <xavierong02@gmail.com>
2026-07-19 08:29:42 +0000

    Jul 19, 2026, 8:29 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -9,21 +9,21 @@ function setup() {
   
 function draw() {
   // Recap 1: Repeating Circles
-  fill(255,0,0); 
-  circle(75,200,100);
+  // fill(255,0,0); 
+  // circle(75,200,100);
 
-  fill(0,255,0);
-  circle(225,200,100);
+  // fill(0,255,0);
+  // circle(225,200,100);
 
-  fill(0,0,255);
-  circle(375,200,100);
+  // fill(0,0,255);
+  // circle(375,200,100);
 
-  fill(100,24,75);
-  circle(525,200,100);
+  // fill(100,24,75);
+  // circle(525,200,100);
   
   //to do for loop-> for(let i =0; i<11; i++ ){}
   for(let i=0; i<4;i++){
-    circle(75 + (i*50),200,100);
+    circle(75 + (i*150),200,100);
   }
   // Task 1: Colour Gradient
 
