6c87e56e65eae7131a1ae80d460b8c5f9012abf5
xorh02 <xavierong02@gmail.com>
2026-07-19 09:21:55 +0000

    Jul 19, 2026, 9:21 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -69,5 +69,7 @@ function draw() {
    startX = gap + radius;
    //step 6 create the loop for 5 circle
    for(let i=0;i<5;i++){ circle(startX +i*30,30,30)};
+
+   
   // Task 4: Grid of Circles
 }
