3693cce0c5f5b5af4d3f59e75a4dea23c262408a
xorh02 <xavierong02@gmail.com>
2026-07-19 08:53:44 +0000

    Jul 19, 2026, 8:53 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -45,8 +45,11 @@ function draw() {
   for(let i=0;i<8;i++){
     if(i%2==0){
       fill(0);
+    }else{
+      fill(255);
     }
   }
+  
   // Task 3: Row of Circles
 
   // Task 4: Grid of Circles
