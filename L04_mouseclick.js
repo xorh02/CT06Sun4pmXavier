5089d205024605bfa7ea341d5b59eae7b670283c
xorh02 <xavierong02@gmail.com>
2026-07-26 08:39:30 +0000

    Jul 26, 2026, 8:39 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -13,7 +13,8 @@ function draw(){
     //random(one number); -> generate a number between 0 and one number;
     //random(low number,high number); -> generate a number between lownumber and high number
     //fill(r,g,b);
-    
+
+    rect(10,20,)
 }
 
 function mousePressed(){
