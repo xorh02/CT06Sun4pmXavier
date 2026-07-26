65f5aef0dd0c36fe1d555ae3d60abed2d27f256a
xorh02 <xavierong02@gmail.com>
2026-07-26 08:41:07 +0000

    Jul 26, 2026, 8:41 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -4,8 +4,7 @@ let shapecolor = 'blue';
 function setup(){
     createCanvas(600,400);
     background(220);
-     fill(random(255),random(255),random(255));
-    rect(10,20,20,20);
+  
 }
 
 function draw(){
@@ -15,7 +14,8 @@ function draw(){
     //random(one number); -> generate a number between 0 and one number;
     //random(low number,high number); -> generate a number between lownumber and high number
     //fill(r,g,b);and 0 255
-   
+    fill(random(255),random(255),random(255));
+    rect(10,20,20,20);
 }
 
 function mousePressed(){
