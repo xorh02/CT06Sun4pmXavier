61ca143916090be80ffa2fd83fe45240c18ff785
xorh02 <xavierong02@gmail.com>
2026-07-26 09:00:50 +0000

    Jul 26, 2026, 9:00 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -13,9 +13,7 @@ function draw(){
     //random(one number); -> generate a number between 0 and one number;
     //random(low number,high number); -> generate a number between lownumber and high number
     //fill(r,g,b);and 0 255
-    if(mouseIsPressed){
-        ellipse(mouseX,mouseY,10,10);
-    }
+  
 }
 
 function mousePressed(){
@@ -28,6 +26,6 @@ function mouseReleased(){
     shapecolor = 'white';
     background(220);
 }
-// function mouseMoved(){
-//     ellipse(mouseX,mouseY,10,10);
-// }
+function mouseMoved(){
+    ellipse(mouseX,mouseY,10,10);
+}
