031ca3a55dca503a03f9cb4b1fdb7e2204cd5cde
xorh02 <xavierong02@gmail.com>
2026-07-26 08:49:41 +0000

    Jul 26, 2026, 8:49 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -20,6 +20,7 @@ function mousePressed(){
     shapecolor = color(random(255),random(255),random(255))
     fill(random(255),random(255),random(255));
     rect(random(600),random(400),random(255),random(255));
+    
 }
 function mouseReleased(){
     shapecolor = 'white';
