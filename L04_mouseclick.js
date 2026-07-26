cce956448c2643e95fc063abb089e74b8b8fa18b
xorh02 <xavierong02@gmail.com>
2026-07-26 08:53:49 +0000

    Jul 26, 2026, 8:53 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -1,13 +1,14 @@
 // write your codes here
 let shapecolor = 'white';
-let number;
+
 function setup(){
     createCanvas(600,400);
     background(220);
   
 }
 function draw(){
-  
+    fill(shapecolor);
+    circle(300,200,300);
     //random(); -> generate a number between 0 and 1
     //random(one number); -> generate a number between 0 and one number;
     //random(low number,high number); -> generate a number between lownumber and high number
@@ -18,16 +19,8 @@ function draw(){
 function mousePressed(){
     shapecolor = color(random(255),random(255),random(255))
     fill(random(255),random(255),random(255));
-    number = random(3);
-    if(number ===0){
-        circle(random(255),random(255),random(255));
-    }else if(number ===1){
-        quad(random(255),random(255),random(255),random(255));
-    }else if(number===2){
-        ellipse(random(255),random(255),random(255),random(255));
-    }else if(number ===3){
-        rect(random(255),random(255),random(255),random(255));
-    }
+    rect(random(600),random(400),random(255),random(255));
+    
 }
 function mouseReleased(){
     shapecolor = 'white';
