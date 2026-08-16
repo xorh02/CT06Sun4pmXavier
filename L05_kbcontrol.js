9aa1eb730e9073a74b9f137b81b719e35bc606bb
xorh02 <xavierong02@gmail.com>
2026-08-16 08:21:33 +0000

    Aug 16, 2026, 8:21 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -20,7 +20,7 @@ function mouseDragged(){
 function keyPressed(){ 
     background(220);
     let numCircles = floor(random(5,21));
-    for(let i =0; i < numCircles;++i){
+    for(let i =0; i < numCircles;i++){
         let x = random(width);
         let y = random(height);
         let size = random(5,40);
