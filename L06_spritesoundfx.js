f57beec3378def4723d9bfb5da47f1d6e1408171
xorh02 <xavierong02@gmail.com>
2026-08-23 08:24:24 +0000

    Aug 23, 2026, 8:24 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -1,8 +1,5 @@
 // write your codes here
-let x= 200;
-let y=200;
 let soundEffect, bgMusic, staticImage;
-
 function preload(){
     soundEffect = loadSound('assets/pop.mp3');
     bgMusc = loadSound('assets/bossaNova.mp3');
@@ -18,17 +15,5 @@ function setup(){
 
 function draw(){
   
-    // if(keyCode === 38){
-    //   y -= 5;
-    // }
-    // if(keyCode === 37){
-    //     x -=5;
-    // }
-    // if(keyCode === 39){
-    //    x += 5;
-    // }
-    // if(keyCode ===40){
-    //   y +=5;
-    // }
-    // circle(x,y,50);
+  
 }
